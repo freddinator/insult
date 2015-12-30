866c78d351751be02405da3e07f713e3f5a6aecd
Freddy Heppell <f.heppell@gmail.com>
2015-12-30 11:42:54 +0000

    Completed bs4 migration

--- index.php
+++ index.php
@@ -28,20 +28,37 @@ $copyInsult = "Thou art $indefArt $words[0] $words[1] $words[2]"
 	<title>Insult Generator</title>
 
 	<!-- Bootstrap core CSS -->
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<style>
 		body {
-			padding-top: 50px;
+			/*padding-top: 50px;*/
+			margin-bottom: 120px;
 		}
 		.starter-template {
 			padding: 40px 15px;
 			text-align: center;
+			white-space: pre-wrap;
 		}
 
 		.buttons {
 			padding-top:30px;
 		}
+
+		html {
+			position: relative;
+			min-height: 100%;
+		}
+		.footer {
+			position: absolute;
+			bottom: 0;
+			width: 100%;
+			/* Set the fixed height of the footer here */
+			height: 120px;
+			line-height: 60px; /* Vertically center the text there */
+			text-align: center;
+		}
+
 	</style>
 
 
@@ -53,15 +70,21 @@ $copyInsult = "Thou art $indefArt $words[0] $words[1] $words[2]"
 	<div class="container">
 
 		<div class="starter-template">
-			<h1 class="copytext">Thou art <? echo $indefArt;?> <br> <span class="define" title="<?php echo defineWord($words[0]);?>" data-toggle="tooltip" data-placement="bottom"><?echo $words[0];?></span> <span class="define" title="<?php echo defineWord($words[1]);?>" data-toggle="tooltip" data-placement="bottom"><?echo $words[1];?></span> <span class="define" title="<?php echo defineWord($words[2]);?>" data-toggle="tooltip" data-placement="bottom" ><?echo $words[2];?></span></h1>
-			<h4 class="buttons"><a href="./"><i class="fa fa-refresh"></i></a> &bull; <a href="?f=<?php echo $numbers[0];?>&s=<?php echo $numbers[1];?>&t=<?php echo $numbers[2];?>"><i class="fa fa-link"></i></a> &bull; <a href="#" id="copy-button" title="Copied!" data-clipboard-text="<?php echo $copyInsult; ?>"><i class="fa fa-clipboard"></i></a></h4>
-			<small>Words from <a href="http://imgur.com/E1R5fo4" target="_blank">this imgur post</a> &bull; 122500 combinations &bull; Hover for definitions</small>
+			<h1 class="display-1">Thou art <? echo $indefArt;?> <br> <span class="define" title="<?php echo defineWord($words[0]);?>" data-toggle="tooltip" data-placement="bottom"><?echo $words[0];?></span> <span class="define" title="<?php echo defineWord($words[1]);?>" data-toggle="tooltip" data-placement="bottom"><?echo $words[1];?></span> <span class="define" title="<?php echo defineWord($words[2]);?>" data-toggle="tooltip" data-placement="bottom" ><?echo $words[2];?></span></h1>
 		</div>
 
 	</div><!-- /.container -->
 
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
-	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<footer class="footer">
+		<div class="container">
+			<h4 class="buttons"><a href="./"><i class="fa fa-refresh"></i></a> &bull; <a href="?f=<?php echo $numbers[0];?>&s=<?php echo $numbers[1];?>&t=<?php echo $numbers[2];?>"><i class="fa fa-link"></i></a> &bull; <a href="#" id="copy-button" title="Copied!" data-clipboard-text="<?php echo $copyInsult; ?>"><i class="fa fa-clipboard"></i></a></h4>
+			<small>Words from <a href="http://imgur.com/E1R5fo4" target="_blank">this imgur post</a> &bull; 122500 combinations &bull; Hover for definitions</small>
+		</div>
+	</footer>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.1.1/js/tether.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.4.2/clipboard.min.js"></script>
 
 	<script type="text/javascript">
@@ -82,7 +105,7 @@ $copyInsult = "Thou art $indefArt $words[0] $words[1] $words[2]"
 		});
 
 		$(function () {
-  			$('[data-toggle="tooltip"]').tooltip()
+			$('[data-toggle="tooltip"]').tooltip()
 		});
 	</script>
 
